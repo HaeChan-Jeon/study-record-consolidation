7123946c1a96d2b6d3f2c2d39e90cfcb87baa221
HaeChan Jeon <146603024+HaeChan-Jeon@users.noreply.github.com>
2023-12-19 23:36:36 +0900

    Add files via upload

---
+++ javascript/vanilla/querySelector/app.js
@@ -0,0 +1,12 @@
+const titleOne = document.querySelector(".hello h1");
+
+const titleTwo = document.querySelector(".hello:nth-child(2) h1");
+
+const titleThree = document.querySelector("#hello");
+
+const titleAll = document.querySelectorAll(".hello h1");
+
+console.log(titleOne);
+console.log(titleTwo);
+console.log(titleThree);
+console.log(titleAll);
