2a29f69466a9fdbb9bc5807dd27d59abd02ecf7f
HaeChan Jeon <146603024+HaeChan-Jeon@users.noreply.github.com>
2024-04-04 16:56:28 +0900

    Add files via upload

---
+++ ReactJS/01_react-for-beginners/23_coin tracker/src/App.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [conins, setConins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json())
+    .then((json) => setConins(json));
+    setLoading(false);
+  }, [])
+  return <div>
+    <h1>The Coins! {loading ? "" : `(${conins.length})`}</h1>
+    {loading ? (<strong>Loading...</strong>
+    ): (
+    <select>
+      {conins.map((coin) => <option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
+    </select>
+    )}
+  </div>;
+}
+
+export default App;
